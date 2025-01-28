427ceeab1b1bacf6b44e1c13be596ab07e3ed6e9
DOmenickCAstillo <castillodomenick2@gmail.com>
2025-01-28 09:27:59 -0800

    added new page and added message to mailto

--- js/mailto.js
+++ js/mailto.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('apply-now-btn');
 
 const sendEmail = () => {
-   window.open('mailto:pallevar.me@gmail.com');
+   window.open('mailto:pallevar.me@gmail.com?subject=Embedded Hardware Engineer Application&body=Me interesa el empleo para la posición de: Embedded Hardware Engineer');
 }
 
 
